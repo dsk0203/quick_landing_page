250e4121835ecea00261fe6d1dda920229335c94
dylan <dylan@example.com>
2021-09-02 22:22:44 -0400

    updating name

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            A newsletter by You
+            Your Newsletter
           </a>{' '}
         </div>
 
